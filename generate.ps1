6a425476dc358bc2924290128f8afd00415ec689
Artyom Romanov <temaromanov@gmail.com>
2025-06-19 17:19:57 +0300

    add all2

--- generate.ps1
+++ generate.ps1
@@ -1,7 +1,7 @@
 ﻿[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 
 python TTS/bin/synthesize.py `
-  --text "Привет, Артём!" `
+  --text "Утро. Сел поработать. Прошло 3 часа — ничего не готово" `
   --model_name tts_models/multilingual/multi-dataset/xtts_v2 `
   --speaker_wav "./my_voice.wav" `
   --language_idx "ru"
