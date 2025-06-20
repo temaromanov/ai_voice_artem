9d772bf62266e21d1825c327320339b88906ccce
Artyom Romanov <temaromanov@gmail.com>
2025-06-20 10:04:08 +0300

    add voices

--- generate.ps1
+++ generate.ps1
@@ -1,9 +1,28 @@
 ﻿[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 
-python TTS/bin/synthesize.py `
-  --text "Утро. Сел поработать. Прошло 3 часа — ничего не готово" `
-  --model_name tts_models/multilingual/multi-dataset/xtts_v2 `
-  --speaker_wav "./my_voice.wav" `
-  --language_idx "ru"
+# Общий текст
+$text = "Утро. Сел поработать. Прошло 3 часа — ничего не готово."
+
+# Папка с голосами
+$voices = @(
+    "voices\avinov_sergey.wav",
+    "voices\maliiv_sergey.wav",
+    "voices\germash_denis.wav",
+    "voices\Ilia_prudovskiy.wav",
+    "voices\lihorad_oleg.wav"
+)
+
+foreach ($voice in $voices) {
+    $name = [System.IO.Path]::GetFileNameWithoutExtension($voice)
+    Write-Host "`n ▶️ Генерация голоса: $name"
+
+    python TTS/bin/synthesize.py `
+        --text "$text" `
+        --model_name "tts_models/multilingual/multi-dataset/xtts_v2" `
+        --speaker_wav "$voice" `
+        --language_idx "ru" `
+        --out_path "tts_output_$name.wav"
+}
+
 
 
